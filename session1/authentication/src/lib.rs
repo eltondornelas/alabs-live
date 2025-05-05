9a4c661f55774d767b49705e54e73769f7d28cf4
Elton Dornelas <eltondornelas@gmail.com>
2025-05-05 13:25:18 -0400

    creating a basic login system

--- session1/authentication/src/lib.rs
+++ session1/authentication/src/lib.rs
@@ -2,6 +2,19 @@ pub fn greet_user(name: &str) -> String {
     format!("Hello {name}")
 }
 
+pub fn login(username: &str, password: &str) -> bool {
+    username.to_lowercase() == "admin" && password == "password"
+}
+
+pub fn read_line() -> String {
+    let mut input: String = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Stdin not working");
+
+    input.trim().to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,6 +22,14 @@ mod tests {
     #[test]
     fn test_greet_user() {
         assert_eq!("Hello Dornelas", greet_user("Dornelas"));
+    }
+
+    #[test]
+    fn test_login() {
+        assert!(login("admin", "password"));
+        assert!(login("ADMIN", "password"));
+        assert!(!login("abc", "def"));
+        assert!(!login("123", "456"));
     }
 }
 
--- session1/login/src/main.rs
+++ session1/login/src/main.rs
@@ -1,5 +1,26 @@
-use authentication::greet_user;
+use authentication::{greet_user, login, read_line};
 
 fn main() {
-    println!("{}", greet_user("Dornelas"))
+    let mut tries: i32 = 0;
+
+    loop {
+        println!("Enter your username:");
+        let username: String = read_line();
+
+        println!("Enter your password:");
+        let password: String = read_line();
+
+        if login(&username, &password) {
+            println!("Welcome");
+            break;
+        } else {
+            println!("Incorrect username or password");
+            tries += 1;
+
+            if tries >= 3 {
+                println!("Too many failed logins");
+                break;
+            }
+        }
+    }
 }
