12b80604ced3115488aaa260a2f462a30adbab37
Elton Dornelas <eltondornelas@gmail.com>
2025-05-05 13:14:12 -0400

    creating login project with dependency of authentication lib

--- session1/authentication/src/lib.rs
+++ session1/authentication/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn greet_user(name: &str) -> String {
+    format!("Hello {name}")
 }
 
 #[cfg(test)]
@@ -7,9 +7,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_greet_user() {
+        assert_eq!("Hello Dornelas", greet_user("Dornelas"));
     }
 }
 
