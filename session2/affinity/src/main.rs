40f6baf2e0b42c810ace6cd3fedb980fd5b60b1d
Elton Dornelas <eltondornelas@gmail.com>
2025-05-12 14:55:42 -0400

    learning about priority

--- session2/affinity/src/main.rs
+++ session2/affinity/src/main.rs
@@ -1,19 +1,23 @@
 fn main() {
     let core_ids = core_affinity::get_core_ids().unwrap();
 
-    let handlers = core_ids.into_iter().map(|id| {
-        std::thread::spawn(move || {
-            let success = core_affinity::set_for_current(id); // some OP might not let you put into affinity to CPU core 0 (MACOS unless you are running in root) since quite a lot of system program runs there
-            if success {
-                println!("Hello from a thread on core {id:?}");
-            } else {
-                println!("Unnable to set affinity to core {id:?}");
-            }
-
+    let handlers = core_ids
+        .into_iter()
+        .map(|id| {
+            std::thread::spawn(move || {
+                let success = core_affinity::set_for_current(id); // some OP might not let you put into affinity to CPU core 0 (MACOS unless you are running in root) since quite a lot of system program runs there
+                if success {
+                    println!("Hello from a thread on core {id:?}");
+                } else {
+                    println!("Unnable to set affinity to core {id:?}");
+                }
+            })
         })
-    }).collect::<Vec<_>>(); // _ = doens't matter the type it gives
+        .collect::<Vec<_>>(); // _ = doens't matter the type it gives
 
     for handle in handlers.into_iter() {
         handle.join().unwrap();
     }
 }
+
+// affinity is binding a thread to either be locked to or making it to prefer one of the CPU cores
