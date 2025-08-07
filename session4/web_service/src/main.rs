56294e8e3448cf357be9c34f714c61ac7d3d976d
Elton Dornelas <eltondornelas@gmail.com>
2025-08-07 18:26:25 -0400

    creating and testing endpoins request and return

--- session4/web_service/src/main.rs
+++ session4/web_service/src/main.rs
@@ -1,9 +1,17 @@
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    routing::{get, post},
+};
 // use std::net::SocketAddr;
+use axum::response::Html;
+use serde::Serialize;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(say_hello_text));
+    let app = Router::new()
+        .route("/", get(say_hello_text))
+        .route("/json", get(hello_json))
+        .route("/post", post(hello_post)); // without the () it's a fuction pointer
     // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -14,8 +22,32 @@ async fn main() {
         .unwrap(); // since not using anyhow
 }
 
-async fn say_hello_text() -> &'static str {
-    "Hello, world!"
+#[derive(Serialize)]
+struct HelloJson {
+    message: String,
+}
+
+async fn hello_json() -> axum::Json<HelloJson> {
+    let message = HelloJson {
+        message: "Hi from JSON".to_string(),
+    };
+    axum::Json(message)
+}
+
+// async fn say_hello_text() -> Html<&'static str> {
+async fn say_hello_text() -> Html<String> {
+    let path = std::path::Path::new("./src/hello.html");
+    let content = tokio::fs::read_to_string(path).await.unwrap();
+    Html(content)
+
+    // const HTML: &str = include_str!("hello.html");
+    // Html(HTML)
+    // Html("<h1>Hello, world!</h1>")
+}
+
+async fn hello_post() -> Html<String> {
+    Html("Hello from post".to_string())
 }
 
 // cargo add axum
+// https://github.com/thebracket/ArdanUltimateRust-5Days/blob/main/03-Async/Axum.md
