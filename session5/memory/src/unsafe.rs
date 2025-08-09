fb964760d6fec1f2751cf99b2a26e1a5fb432ace
Elton Dornelas <eltondornelas@gmail.com>
2025-08-09 10:37:21 -0400

    examples with unsafe and memory allocation

---
+++ session5/memory/src/unsafe.rs
@@ -0,0 +1,29 @@
+// /// # Safety
+// /// 
+// /// This function is unsafe because...
+// unsafe fn my_fn() {
+
+// }
+
+// fn main() {
+//     let my_vec = vec![1,2,3,4,5];
+//     // let my_vec: Result<Vec<i32>, String> = Ok(vec![1,2,3,4,5]);
+//     // println!("{}", my_vec[20]);
+    
+//     // if let Ok(v) = my_vec {
+//     //     println!("{:?}", v.get(2).unwrap());
+        
+//     // }
+    
+//     // unsafe {
+//         if let Ok(value) = my_vec.get_unchecked(2) {
+//             println!("{}", value)
+//         }
+//     // } 
+
+//     /** 
+//      * when your program is created you are giving a stack and there is a heap which is a big blob of memory
+//      * and stack is tiny by default 64Kb on linus
+//      */
+   
+// }
